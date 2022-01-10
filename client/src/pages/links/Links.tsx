e10733ce3a97fffac253546d3125e8987b39d2de
Johan Castillo <jcjohan2707@gmail.com>
2022-01-10 16:10:14 -0400

    Links

---
+++ client/src/pages/links/Links.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Links() {
+    return (
+        <div>
+            <a href="">
+                Whatsapp
+            </a>
+
+            <a href="">
+                Pagina Web
+            </a>
+        </div>
+    )
+}
